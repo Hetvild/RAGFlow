69990e56ada25ea638d706434b9df5c3c76e3953
bil <hetvild@mail.ru>
2026-02-24 13:49:02 +0500

    refactor: перенес в lifespan открытие и закрытие qdrant postrgres для aiogram

--- src/app.py
+++ src/app.py
@@ -6,16 +6,24 @@ from fastapi import FastAPI, Request
 
 from core.config import settings
 from core.logging import logger
+from core.qdrant import qdrant_manager
 from core.redis import redis_manager
+from db.db_helper import db_helper
 from integrations.telegram.bot import bot
 from integrations.telegram.handlers.commands import command_router
 from integrations.telegram.handlers.messages import messages_router
+from integrations.telegram.middlewares.db import DatabaseMiddleware
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     logger.info("Приложение запущено...")
+
+    # Открываем подключение к Redis
     await redis_manager.connect()
+
+    # Открываем подключение к Qdrant
+    await qdrant_manager.connect()
 
     storage = RedisStorage(
         redis=redis_manager.get_client(),
@@ -25,6 +33,8 @@ async def lifespan(_: FastAPI):
 
     # Создаем новый диспетчер с RedisStorage
     dp = Dispatcher(storage=storage)
+
+    dp.update.middleware(DatabaseMiddleware())
 
     # Подключаем роутеры
     dp.include_router(command_router)
@@ -47,7 +57,14 @@ async def lifespan(_: FastAPI):
 
     # Закрываем соединение с ботом
     await bot.session.close()
+
+    # Закрываем подключение к Redis
     await redis_manager.disconnect()
+
+    # Закрываем подключение к Qdrant
+    await qdrant_manager.disconnect()
+
+    await db_helper.dispose()  # <--- Добавляем закрытие пула Postgres
 
 
 app = FastAPI(
@@ -82,4 +99,3 @@ async def bot_webhook(request: Request):
         update,
     )
     return {"ok": True}
-
