829d178bf30b5d105b44e3317178fd4e51d8b2af
bil <hetvild@mail.ru>
2026-02-28 14:34:51 +0500

    feat: добавил хендлер для входа в профиль и лимитов

---
+++ src/integrations/telegram/handlers/profile.py
@@ -0,0 +1,38 @@
+from contextlib import suppress
+
+from aiogram import F, Router, types
+from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramBadRequest  # Импортируем ошибку
+
+from integrations.telegram.keyboards.inline import MenuCallback, get_main_menu_keyboard
+
+
+profile_router = Router()
+
+
+@profile_router.callback_query(MenuCallback.filter(F.action == "profile"))
+async def process_profile_callback(
+    callback: types.CallbackQuery, callback_data: MenuCallback
+):
+    await callback.answer()
+
+    with suppress(TelegramBadRequest):
+        await callback.message.edit_text(
+            text=f"👤 <b>Профиль</b>\n\nID: {callback.from_user.id}\nИмя: {callback.from_user.username}",
+            reply_markup=get_main_menu_keyboard(),
+            parse_mode=ParseMode.HTML,
+        )
+
+
+@profile_router.callback_query(MenuCallback.filter(F.action == "limits"))
+async def process_limits_callback(
+    callback: types.CallbackQuery, callback_data: MenuCallback
+):
+    await callback.answer()
+
+    with suppress(TelegramBadRequest):
+        await callback.message.edit_text(
+            text="📊 <b>Ваши лимиты</b>\n\nОстаток токенов: 1000",
+            reply_markup=get_main_menu_keyboard(),
+            parse_mode=ParseMode.HTML,
+        )
