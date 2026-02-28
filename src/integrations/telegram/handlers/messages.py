33bfc78cfa534d0d172992b7a7c2ac684d5ba035
bil <hetvild@mail.ru>
2026-02-28 14:38:02 +0500

    refactor: убрал обращение к llm и добавил отображение кнопки меню

--- src/integrations/telegram/handlers/messages.py
+++ src/integrations/telegram/handlers/messages.py
@@ -1,14 +1,14 @@
-from aiogram import Router, types
+from aiogram import F, Router, types
 from aiogram.enums import ParseMode
 
 from core.logging import logger
-from services.llm_service import llm_service
+from integrations.telegram.keyboards.inline import get_main_menu_keyboard
 
 
 messages_router = Router()
 
 
-@messages_router.message()
+@messages_router.message(F.text)
 async def cmd_start(message: types.Message):
     user_id = message.from_user.id
     user_name = message.from_user.username
@@ -26,12 +26,8 @@ async def cmd_start(message: types.Message):
         message_text,
     )
 
-    # response = await llm_service.generate_response(
-    #     system_prompt="Ты полезный ассистент. Отвечай кратко.",
-    #     user_message=message.text,
-    #     # context="RAG (Retrieval-Augmented Generation) — это подход, который комбинирует поиск информации с генерацией текста.",
-    # )
-
     await message.answer(
-        text="Тут вернется ответ от llm", parse_mode=ParseMode.MARKDOWN
+        text="Выберете из меню что вы хотите сделать",
+        reply_markup=get_main_menu_keyboard(),
+        parse_mode=ParseMode.MARKDOWN,
     )
